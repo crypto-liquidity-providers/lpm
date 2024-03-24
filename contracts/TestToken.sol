0c4385fdf5ce5c4578a4a255327ccc860c5d7a75
Guilherme Dantas <guidanoli@proton.me>
2024-03-24 02:05:09 -0300

    feat: add `tokenOwner` to `TestToken` constructor

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.24;
 import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor(uint256 totalSupply) ERC20("Test", "TST") {
-        _mint(msg.sender, totalSupply);
+    constructor(address tokenOwner, uint256 totalSupply) ERC20("Test", "TST") {
+        _mint(tokenOwner, totalSupply);
     }
 }
