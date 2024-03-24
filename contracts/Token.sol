bfd810ed92fc49a8218c6da78c2872aab4b9f79e
Guilherme Dantas <guidanoli@proton.me>
2024-03-23 22:34:28 -0300

    style: DummyToken -> Token

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,8 +4,8 @@ pragma solidity 0.8.24;
 
 import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
-contract DummyToken is ERC20 {
-    constructor(uint256 totalSupply) ERC20("DummyToken", "DUM") {
+contract Token is ERC20 {
+    constructor(uint256 totalSupply) ERC20("Token", "TOK") {
         _mint(msg.sender, totalSupply);
     }
 }
